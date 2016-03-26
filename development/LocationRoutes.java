b29447c909dbe1b533b7eaae7cca2275c7126fa6
samt95 <samt.95@live.ca>
2016-03-25 20:40:30 -0700

    LocationRoutes

--- development/LocationRoutes.java
+++ development/LocationRoutes.java
@@ -3,7 +3,7 @@ import javax.servlet.*;
 import javax.servlet.http.*;
 import java.sql.*;
 
-public class AirlineRoutes extends HttpServlet {
+public class LocationRoutes extends HttpServlet {
     void processRequest(HttpServletRequest request, HttpServletResponse response)
     										throws ServletException, IOException {
       response.setContentType("text/html;charset=UTF-8");
@@ -14,13 +14,18 @@ public class AirlineRoutes extends HttpServlet {
       try { 
     	  Statement stmt = conn.createStatement();
     	  ResultSet rset = stmt.executeQuery(
-                        "SELECT * " +
-                        "FROM Routes " +
-                        "WHERE acode = ( " +
-                            "SELECT acode " +
-                            "FROM Airlines " +
-                            "WHERE name='" + airline + "'" +
-                            " )"
+                              "SELECT * "
+                            + "FROM Routes "
+                            + "WHERE rnum IN ( "
+                            + "  SELECT rnum "
+                            + "  FROM OutgoingRoutes "
+                            + "  WHERE destination='Toronto' "
+                            + "  ) "
+                            + "  OR rnum IN ( "
+                            + "  SELECT rnum "
+                            + "  FROM IncomingRoutes "
+                            + "  WHERE source='Toronto' "
+                            + "  )"
                         );
       
     	  out.println("<table>");
