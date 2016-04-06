b71384c4ae62121cf906d0c266c4f09424515a5a
samt95 <samt.95@live.ca>
2016-04-06 12:06:27 -0700

    restructuring changes

--- README.txt
+++ README.txt
@@ -1,1 +1,18 @@
-u w0ttttt
+CSC 370 - Databases Final Project
+
+Airport Database Manager using HTML forms, Tomcat, and Oracle SQL.
+
+---------------------------------------------------------------------
+
+apache-tomcat-6.0.18 was used for this project and must be in the main project folder for proper use.
+
+
+Use:
+
+- copy HTML files to apache-tomcat-6.0.18/webapps/ROOT
+
+- compile the servlets (java) into apache-tomcat-6.0.18/webapps/ROOT/WEB-INF/classes
+
+- start tomcat using the startup.sh script in apache-tomcat-6.0.18/bin
+
+- go to http://localhost:8081/AirportHome.html in your favorite web browser
--- SQL/constraints.sql
+++ SQL/constraints.sql
@@ -1,71 +1,68 @@
---constraints
+-- constraint i
 
-CREATE OR REPLACE VIEW DeparturesView AS
-  SELECT *
-  FROM Departures D 
+CREATE OR REPLACE VIEW PassengersV AS 
+  SELECT * --i.e. all attributes are made available
+  FROM Passengers X
   WHERE NOT EXISTS (
-    SELECT *
-    FROM Departures d1 NATURAL JOIN OutgoingRoutes o1
+    --arrival time is greater than departure time for the tuple from X
     
-    WHERE d1.gate IN (
-      SELECT d2.gate
-      FROM Departures d2 NATURAL JOIN OutgoingRoutes o2
-      WHERE abs(o1.outT-o2.outT) < 1
-    )
-    
-    OR d1.gate IN (
-      SELECT a1.gate
-      FROM Arrivals a1 NATURAL JOIN IncomingRoutes i1
-      WHERE abs(o1.outT-i1.incT) < 1
-    )
-  )
+      Select * 
+      FROM Arrivals, Departures
+      Where X.ARRID = arrID
+      and X.DEPID = depID
+      and arrT > depT
+ 
+ 
+  );
 WITH CHECK OPTION;
 
---AUSTIN
+
+
+
+-- constraint ii
+
 CREATE OR REPLACE VIEW DepartureV AS 
-  SELECT depID,depT,gate,acode,rnum   --i.e. all attributes are made available
+  SELECT *   --i.e. all attributes are made available
   FROM Departures X
   WHERE NOT EXISTS (
     --Departure with the same gate and depT within +/- 1 hour of X.depT
-
     (SELECT *
     FROM Departures d 
     where  X.gate = d.gate
-    and (24*abs(X.depT - d.depT)) < 1)
-
+    and (24*abs(d.depT - X.depT) < 1)
+    and d.depID<>X.depID
+    )
     UNION
     (SELECT *
       FROM Arrivals a
       where a.gate = X.gate
-      and (24*abs(a.arrT - X.depT)) < 1)
+      and (24*abs(a.arrt - X.depT) < 1)
+    --Arrival with the same gate and arrT within +/- 1 hour of X.depT
+    )
+  )
+WITH CHECK OPTION;
 
+
+
+CREATE OR REPLACE VIEW ArrivalV AS 
+  SELECT *   --i.e. all attributes are made available
+  FROM Arrivals X
+  WHERE NOT EXISTS (
+    (SELECT *
+    FROM Departures d 
+    where  X.gate = d.gate
+    and (24*abs(d.depT - X.arrT) < 1)
+  )
+    UNION
+    (SELECT *
+      FROM Arrivals a
+      where a.gate = X.gate
+      and (24*abs(a.arrt - X.arrT) < 1)
+      and a.arrID<>X.arrID
+      )
     --Arrival with the same gate and arrT within +/- 1 hour of X.depT
   )
 WITH CHECK OPTION;
 
 
-CREATE OR REPLACE VIEW DeparturesView AS
-  SELECT *
-  FROM Departures 
-  WHERE NOT EXISTS (
-    SELECT *
-    FROM Departures d1 NATURAL JOIN OutgoingRoutes o1
-    
-    WHERE d1.gate IN (
-      SELECT d2.gate
-      FROM Departures d2 NATURAL JOIN OutgoingRoutes o2
-      WHERE abs(o1.outT-o2.outT) < 1 AND d1.gate<>d2.gate
-    )
-    
-    OR d1.gate IN (
-      SELECT a1.gate
-      FROM Arrivals a1 NATURAL JOIN IncomingRoutes i1
-      WHERE abs(o1.outT-i1.incT) < 1 AND a1.gate<>i1.gate
-    )
-  )
-WITH CHECK OPTION;
 
-
-
-
-check on gates boolean
